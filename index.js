a945dcaa7b4144e2ca09a02b93c236459f1ba494
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-06-12 05:34:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 // Code your solution here
 
-function findMatching(){}
+function findMatching(arr, name){
+  return arr.filter(el => el.toLowerCase() === name.toLowerCase())
+}
 
-function fuzzyMatch(){}
+function fuzzyMatch(arr, name){
+  return arr.filter(el => el.charAt(0) === name.charAt(0))
+}
 
-function matchName(){}
+function matchName(arr, name){
+  return arr.filter(el => el.name === name)
+}
