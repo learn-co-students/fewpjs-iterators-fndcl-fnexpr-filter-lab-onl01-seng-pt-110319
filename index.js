391e7487066092073af6f25d01c167ae425045a1
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-06-21 01:18:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers, name) {
+  return drivers.filter(driver => driver.toUpperCase() === name.toUpperCase());
+}
+
+function fuzzyMatch(drivers, name) {
+  return drivers.filter(driver => driver[0].toUpperCase() === name[0].toUpperCase());
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(driver => driver.name === name)
+}
