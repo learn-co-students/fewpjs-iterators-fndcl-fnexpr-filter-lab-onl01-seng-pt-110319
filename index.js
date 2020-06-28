d9d7cf2b622f67ae4729512dd293d79c9e07ba3f
Mug <brad.alexander913@gmail.com>
2020-06-28 10:36:43 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(array,string){
+let matches =  array.filter(line => {
+    return line.toLowerCase() === string.toLowerCase();
+})
+return matches
+}
+
+function fuzzyMatch(array,string){
+    return array.filter(line => 
+        line.toLowerCase().indexOf(string.toLowerCase()) === 0       
+    )
+}
+
+function matchName(array,string){
+    return array.filter(line => 
+        line.name === string)
+        
+}
